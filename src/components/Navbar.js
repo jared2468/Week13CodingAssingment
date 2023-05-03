f4f78a9816e67bfa194e08ec5cf28e2013a7d2ad
Jared Hollingsworth <123146317+jared2468@users.noreply.github.com>
2023-05-02 23:04:10 -0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../index.css"
+
+function Navbar(){
+  return (
+    <div>
+      <nav className="navbar">
+      <ul>
+          <li className='li'>Home</li>
+          <li>link 1</li>
+          <li>link 2</li>
+      </ul>
+    </nav>
+  </div>
+  );
+}
+export default Navbar;
